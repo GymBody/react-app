e4bb584540555f637c116b9281c6438c112b6fe2
Ahmed Al Salih <a.alsalih2@gmial.com>
2025-01-20 05:51:08 -0500

    ADD SAMPLE IMPLEMENTATION TO THE LOGIN PAGE

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,20 @@
 
 const Login = () => {
 
+    const authToken = true
+
+    const handleClick =() => {
+        console.log('clicked')
+    }
+
     return (
         <div>
+            <h1>
+                Swipe Right
+            </h1>
+            <button onClick={handleClick}>
+                {authToken ? 'Signout' : 'Create AccountE'}
+            </button>
 
         </div>
     )
